b9975eae06cfb378bad31a3d439702b2bd2a601b
Sushant Kumar Tiwari <sushant22tiwari801@gmail.com>
2025-08-07 12:33:02 +0530

    nativewind setup

--- App.tsx
+++ App.tsx
@@ -1,10 +1,11 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import FoodLogo from "./assets/FoodLogo";
+import "./global.css";
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <View className="flex-1 justify-center items-center">
       <FoodLogo />
       <StatusBar style="auto" />
     </View>
